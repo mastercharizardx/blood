f9611ac870a10e9583e83943e909420ccafc29fa
Sikha99 <48063532+Sikha99@users.noreply.github.com>
2020-06-11 10:30:21 +0530

    Update table.php

--- table.php
+++ table.php
@@ -1,5 +1,9 @@
 <?php
 
+require 'vendor/autoload.php';
+?>
+<?php
+
  $host= "ec2-18-210-214-86.compute-1.amazonaws.com";
  $dbUsername = "xjiqxvbnompbzr";
  $dbPassword = "fc254a04a16cff74242a67d7198f88eba25aa5c56a9f2d7852f2c6a11dbd6910";
